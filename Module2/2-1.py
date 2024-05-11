3200f375d96f30ff10e5c836ef7bb34c1d8e231a
Your NameHoward <l78371@gmail.com>
2024-05-11 16:43:34 +0800

    class file

---
+++ Module2/2-1.py
@@ -0,0 +1,15 @@
+# 建立第一支 Python 程式
+
+# 宣告變數
+x = 10
+my_name = 'Howard'
+
+# 輸出變數
+''''''
+print(x)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
+print(my_name)
---
+++ Module3/practice.py
@@ -0,0 +1,31 @@
+# 終極密碼 讓使用者能夠重複猜數字，直到猜對為止
+password = 25
+
+# 1.告訴使用者需要輸入的數字範圍 input()
+while True:
+    num = int(input("請輸入數字1~100:"))
+    if num == password:
+        print("恭喜中獎")
+        break
+    elif num > 100 or num < 1:
+        print("超出範圍請重新輸入")
+        continue
+    elif num > password:
+        print("請輸入更小的數字")
+        continue
+    elif num < password:
+        print("請輸入更大的數字")
+        continue
+
+# 2.使用者猜對要回傳「恭喜中獎」，並結束迴圈的執行
+
+
+# 3.超出範圍要顯示「超出範圍請重新輸入」
+
+
+# 4.數字太大 要提示「請輸入更小的數字」
+
+
+# 5.數字太小 要提示「請輸入更大的數字」
+
+
